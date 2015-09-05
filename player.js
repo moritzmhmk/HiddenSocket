c17cf8b34cca7e0d72f453463ecaffe531259210
moritzmhmk <moritzmhmk@gmail.com>
2015-09-05 19:24:47 +0200

    some input

--- player.js
+++
@@ -1,8 +0,0 @@
-Player = function(socket) {
-  this.socket = socket;
-  this.pos = {x: 0, y: 0};
-};
-
-Player.prototype.move = function(vx, vy, dt) {
-
-}
---
+++ static/player.js
@@ -0,0 +1,8 @@
+Player = function() {
+  this.pos = {x: 0, y: 0};
+};
+
+Player.prototype.move = function(delta) {
+  this.pos.x+=delta.x;
+  this.pos.y+=delta.y;
+}
