7ee3a90fd5e2f31df0bcd7a04daa9aff975b314d
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2015-09-14 14:19:00 +0200

    Adhere to some js standard

--- static/game.js
+++ static/game.js
@@ -1,32 +1,32 @@
-var player = new Player();
+var player = new Player()
 
-document.onkeydown = function(event) {
-  switch(event.keyCode) {
-    case 37: //LEFT
-      player.move({x: -1, y: 0});
-      break;
-    case 38: //UP
-      player.move({x: 0, y: -1});
-      break;
-    case 39: //RIGHT
-      player.move({x: 1, y: 0});
-      break;
-    case 40: //DOWN
-      player.move({x: 0, y: 1});
-      break;
+document.onkeydown = function (event) {
+  switch (event.keyCode) {
+    case 37: // LEFT
+      player.move({x: -1, y: 0})
+      break
+    case 38: // UP
+      player.move({x: 0, y: -1})
+      break
+    case 39: // RIGHT
+      player.move({x: 1, y: 0})
+      break
+    case 40: // DOWN
+      player.move({x: 0, y: 1})
+      break
   }
 }
 
-var lastRender = Date.now();
-function render() {
-  var dt = Date.now() - lastRender;
-  var fps = 1000 / dt;
-  console.log("fps: " + fps);
+var lastRender = Date.now()
+function render () {
+  var dt = Date.now() - lastRender
+  var fps = 1000 / dt
+  console.log('fps: ' + fps)
 
-  window.requestAnimationFrame(render);
+  window.requestAnimationFrame(render)
 
-  document.getElementById('local').style.left = player.pos.x * 10;
-  document.getElementById('local').style.top = player.pos.y * 10;
+  document.getElementById('local').style.left = player.pos.x * 10
+  document.getElementById('local').style.top = player.pos.y * 10
 
-  lastRender += dt;
+  lastRender += dt
 }
