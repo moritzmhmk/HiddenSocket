1568b97e4cf27c18fe3a7019fedf6a2869851b22
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2015-09-14 15:19:32 +0200

    More refactoring

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 var express = require('express')
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
-app.use(express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '/static'))
 
+io.on('connection', function (socket) {
+  console.log('a user connected')
+  socket.on('disconnect', function () {
+    console.log('user disconnected')
+  })
+})
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-});
-
-http.listen(3000, function() {
-  console.log('listening on *:3000');
-});
+http.listen(3000, function () {
+  console.log('listening on *:3000')
+})
